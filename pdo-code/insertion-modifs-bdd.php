91a4aea6d1d86c28e57f73dd5ff0f39e821f509e
arthur-eudeline <34448629+arthur-eudeline@users.noreply.github.com>
2018-01-24 15:21:35 +0100

    Update insertion-modifs-bdd.php

--- pdo-code/insertion-modifs-bdd.php
+++ pdo-code/insertion-modifs-bdd.php
@@ -1,14 +1,10 @@
 
-//Tableau qui va stoquer les données que le vont inserer / modifier dans la bdd
 $values = array(
-    "ID"                =>  [ _identifiant de l'item à update_ ]
-    "nom"           =>  $_POST["nom"],
+    [ _nom de variable_ ]   =>  [ _contenu de la variable_ ],
 );
- 
-//On va stoquer la préparation de la requête. C'est un moyen de sécuriser la requête et d'éviter les erreurs. De plus, il est possible d'avoir accès est des "VARIABLES"
-//par exemple : on va choisir le champs, dans la requête d'exemple c'est "nom" et on va lui attribuer la valeur ":nom". Quand on met ":" devant un mot dans un PDO->prepare, cela va tout simplement permettre de dire, je veux que tu utilises la case "nom" d'un tableau que je vais te donner ensuite. Attention, ":nom" pourrait être ":valeur", ":chose", on s'en fou, tout ce qui compte c'est que ce qu'il y a après les ":" soit le nom d'une case du tableau !
 
 $requete = $DB->prepare('UPDATE [ _nom de la table_ ] SET nom = :nom, [ etc...] WHERE ID = :ID;)
-    
-//Là on lui dit d'éxecuter notre requête avec les valeurs stoquées dans le tableau $values
+//ou
+$requete = $DB->prepare('INSERT INTO [ _nom de la table_ ] ([ _nom de colonne 1_ ], [ _nom de colonne 2_ ], ...) VALUES (:[ _variabe_ ], :[ _variabe_ ], ...)');
+
 $requete->execute($values); 
